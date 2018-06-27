9e656303a07b7d6dcc36b25ed29c7d846a1e4265
Wilhelm <wilhelmprins@gmail.com>
2018-06-27 17:03:06 +0200

    Animation
    jumping and running

---
+++ UnityAdvanced/Assets/archer/ArcherScripts/ArcherControls.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcherControls : MonoBehaviour {
+
+    static Animator anim;
+    public float speed = 10.0f;
+    public float rotSpeed = 100.0f;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rot = Input.GetAxis("Horizontal") * rotSpeed;
+        translation *= Time.deltaTime;
+        rot *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rot, 0);
+
+        if (Input.GetButtonDown("Jump") && translation == 0)
+        {
+            anim.SetTrigger("isJump");
+        }
+        else if (Input.GetButtonDown("Jump") && translation > 0)
+        {
+            anim.SetTrigger("isJump");
+        }
+        else if(Input.GetButtonDown("Jump") && translation < 0){
+            anim.SetTrigger("isJumpBack");
+        }
+
+        if (translation > 0)
+        {
+            anim.SetBool("isRun", true);
+            anim.SetBool("isRunBack", false);
+            anim.SetBool("isIdle", false);
+        }
+        else if (translation < 0)
+        {
+            anim.SetBool("isRunBack", true);
+            anim.SetBool("isRun", false);
+            anim.SetBool("isIdle", false);
+        }
+        else {
+            anim.SetBool("isRunBack", false);
+            anim.SetBool("isRun", false);
+            anim.SetBool("isIdle", true);
+        }
+	}
+}
