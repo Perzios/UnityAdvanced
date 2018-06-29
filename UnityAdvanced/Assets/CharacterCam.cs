f2a98d60f47ab17c6243c668092d425f20ac15fa
Wilhelm <wilhelmprins@gmail.com>
2018-06-29 15:37:54 +0200

    Updates
    Better rotation
    Minor collision

---
+++ UnityAdvanced/Assets/CharacterCam.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCam : MonoBehaviour {
+
+    public Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var CharacterRot = cam.transform.rotation;
+        CharacterRot.x = 0;
+        CharacterRot.z = 0;
+        transform.rotation = CharacterRot;
+    }
+}
