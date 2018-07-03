264de4579e8debb781fcc63c00c2619d17438739
Wilhelm <wilhelmprins@gmail.com>
2018-07-03 15:37:38 +0200

    Revert "Updated rotatation Cam"
    This reverts commit 4f8dbedc11fa12838c2a2a98ce7aac1028efb4b8.

--- UnityAdvanced/Assets/RotationCam.cs
+++ UnityAdvanced/Assets/RotationCam.cs
@@ -4,39 +4,35 @@ using UnityEngine;
 
 public class RotationCam : MonoBehaviour {
 
-    private const float Y_ANGLE_MIN = 10.0f;
-    private const float Y_ANGLE_MAX = 50.0f;
-
     public GameObject target;
 
-    public float dist;
-
-    private float currentX = 0.0f;
-    private float currentY = 20.0f;
-    private float sensitivityX = 4.0f;
+    public float dist, height;
+    private float oldDist = 0, oldHeight = 0;
     private float sensitivityY = 0.5f;
 
     // Use this for initialization
     void Start () {
+        height = Mathf.Clamp(height, 2.0f, 5.0f);
+        dist = Mathf.Clamp(dist, 5.0f, 12.0f);
+        Vector3 dir = new Vector3(0, height, -dist);
+        transform.position = target.transform.position + dir;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        currentY += Input.GetAxis("Mouse Y") * sensitivityY;
-        currentX += 15.0f * sensitivityX * Time.deltaTime;
+
+        oldHeight = height;
+        height += Input.GetAxis("Mouse Y") * sensitivityY;
+        oldDist = dist;
         dist -= Input.GetAxis("Mouse ScrollWheel");
-        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);        
+        height = Mathf.Clamp(height, 2.0f, 5.0f);        
         dist = Mathf.Clamp(dist, 5.0f, 12.0f);
-    }
 
+        Vector3 dir = new Vector3(0, height-oldHeight, dist-oldDist);
+        transform.position += dir;
 
-    private void LateUpdate()
-    {
-        Vector3 dir = new Vector3(0, 0, -dist);
-        Quaternion rot;
-        rot = Quaternion.Euler(currentY, currentX, 0);
-        transform.position = target.transform.position + rot * dir;
         transform.LookAt(target.transform.position);
+        transform.RotateAround(target.transform.position, Vector3.up, Time.deltaTime * 25);
     }
-
+    
 }
